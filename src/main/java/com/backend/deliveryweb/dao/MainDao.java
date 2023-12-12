642bc440d0b00b147e84d70b09ed081984dab7cc
bin <sonkuebin@naver.com>
2023-12-12 20:16:55 +0900

    ddd

--- src/main/java/com/backend/deliveryweb/dao/MainDao.java
+++ src/main/java/com/backend/deliveryweb/dao/MainDao.java
@@ -1,4 +1,21 @@
 package com.backend.deliveryweb.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class MainDao {
+
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate = null;
+
+    public List<Map<String, Object>> selectAll(Map<String, Object> pMap) {
+        List<Map<String, Object>> list = sqlSessionTemplate.selectList("selectAll", pMap);
+        return list;
+    }
+
 }
--- src/main/java/com/backend/deliveryweb/logic/MainLogic.java
+++ src/main/java/com/backend/deliveryweb/logic/MainLogic.java
@@ -1,4 +1,23 @@
 package com.backend.deliveryweb.logic;
 
+import com.backend.deliveryweb.dao.MainDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Service
 public class MainLogic {
+
+    @Autowired
+    private MainDao mainDao;
+
+    public List<Map<String, Object>> selectAll(Map<String, Object> pMap) {
+
+        List<Map<String, Object>> list = mainDao.selectAll(pMap);
+
+        return list;
+    }
 }
