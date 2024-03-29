d4e8651889746cd42dcaf65f464c7fa4d0498bd6
bin <sonkuebin@naver.com>
2024-03-29 15:00:38 +0900

    3/29

---
+++ src/main/front/src/components/page/Header.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+function Header(){
+
+    return(
+        <div>
+            <div>
+                사진
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
