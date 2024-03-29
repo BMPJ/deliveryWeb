56a6d4f2db4d1db85ad145642b820f0250e9717f
yg <aypd02150@gmail.com>
2024-03-29 20:06:39 +0900

    3/29

---
+++ src/main/front/src/styles/HeaderStyle.js
@@ -0,0 +1,86 @@
+import styled from "styled-components"
+
+export const HeaderBlock = styled.div`
+  height: 78px;
+  background-color: #fa0050;
+
+  .block {
+    position: relative;
+    max-width: 1020px;
+    max-height: 320px;
+    margin: 0 auto;
+  }
+
+  img {
+    position: absolute;
+    width: 90px;
+    height: 38px;
+    top: 19px;
+  }
+
+  .buttons {
+    position: relative;
+    width: 330px;
+    height: 50px;
+    top: 19px;
+    left: 700px;
+  }
+
+  .logout {
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    color: #fff;
+    border: 1px solid #ea7226;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 2px;
+  }
+
+  .orderList {
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    left: 110px;
+    color: #fff;
+    border: 1px solid #ea7226;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 2px;
+  }
+
+  .login {
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    color: #fff;
+    border: 1px solid #ea7226;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 2px;
+  }
+
+  .join {
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    left: 110px;
+    color: #fff;
+    border: 1px solid #ea7226;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 2px;
+  }
+
+  .manage {
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    left: 220px;
+    color: #fff;
+    border: 1px solid #ea7226;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 2px;
+  }
+`;
