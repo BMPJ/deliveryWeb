1d5b5ba0af05df7da2a42794ee03c8cff053c4c3
yg <aypd02150@gmail.com>
2024-04-22 04:40:41 +0900

    4/22

--- src/main/front/src/styles/MainDelivery.js
+++ src/main/front/src/styles/MainDelivery.js
@@ -1,13 +1,24 @@
 import styled from "styled-components"
 
 export const Main = styled.div`
-    width: 1200px;
+  max-width: 1020px;
+  margin: 0 auto;
 `
 export const Category = styled.div`
-    width: 300px;
-    height: 200px;
-    float: left;
-    border : 1px solid black;
-    margin: 1px;
-    cursor: pointer;
+  position: relative;
+  width: 300px;
+  height: 200px;
+  float: left;
+  border: 1px solid lightgray;
+  margin: 5px;
+  cursor: pointer;
+
+  .name {
+    position: absolute;
+    top: 7%;
+    left: 7%;
+    font-size: 110%;
+    font-weight: bold;
+  }
+  
 `
